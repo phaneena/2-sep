949fde730f270101724b9c8582bdcb18dbcfb7ff
Haneena P <85494776+phaneena@users.noreply.github.com>
2024-09-09 11:22:41 +0530

    Add files via upload

---
+++ async.js
@@ -0,0 +1,73 @@
+// const pr=new Promise((resolve,reject)=>{
+//     resolve('suceesfully completed')
+// })
+// async function getdata(){
+//     const val=await pr;
+//     console.log(val);
+// }
+// getdata();
+
+
+// const pro=new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         resolve('it returns promise')
+//     },10000)
+// });
+
+// async function create(){
+//     let value=await pro;
+//     console.log('namste');
+//     console.log(value);
+// }
+// create();
+
+
+
+// const prom=new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         resolve('successfully going')
+//     },10000)
+// });
+// const pro=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve('pending off')
+//     },5000)
+// });
+
+// async function find(){
+//     console.log('hyy');
+//     let val=await prom;
+//     console.log('hiii');
+//     console.log(val);
+
+//     let val1=await pro;
+//     console.log('hellooo');
+//     console.log(val1)
+// }
+// find();
+
+
+// const pr=new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         resolve('partially completed')
+//     },5000)
+// });
+// const pr1=new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         resolve('successfully completed')
+//     },10000)
+// })
+
+// async function new1(){
+//     let val=await pr;
+//     console.log('good')
+//     console.log(val);
+//     let val1=await pr1;
+//     console.log('happy');
+//     console.log(val1);
+// }
+// new1();
+
+
+
+
